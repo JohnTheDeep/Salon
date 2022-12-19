9a452565da9207fe5be45ea7243879cdd52823fa
Alexander <106916560+JohnTheDeep@users.noreply.github.com>
2022-12-19 21:20:50 +0300

    deleted

--- Salon/Salon/Models/Data/SalonModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Salon.Models.Data
-{
-    public class SalonModel
-    {
-        public int Id { get; set; }
-        public string SalonName { get; set; }
-        public string Director { get; set; }
-    }
-}
